cfbee05b4e387f7b6e342df76aa0ec82a79f0bf1
Michael Babker <michael.babker@gmail.com>
2020-01-14 09:31:36 -0600

    Docs for facade

--- src/Facades/Calendar.php
+++ src/Facades/Calendar.php
@@ -3,9 +3,31 @@
 namespace MaddHatter\LaravelFullcalendar\Facades;
 
 use Illuminate\Support\Facades\Facade;
+use MaddHatter\LaravelFullcalendar\Calendar as RealCalendar;
+use MaddHatter\LaravelFullcalendar\Event;
 
-class Calendar extends Facade
+/**
+ * @method static string calendar()
+ * @method static string script()
+ * @method static RealCalendar setId(string $id)
+ * @method static string getId()
+ * @method static RealCalendar addEvent(Event $event, array $customAttributes = [])
+ * @method static RealCalendar addEvents($events, array $customAttributes = [])
+ * @method static RealCalendar setOptions(array $options)
+ * @method static array getOptions()
+ * @method static RealCalendar setCallbacks(array $callbacks)
+ * @method static array getCallbacks()
+ * @method static string getOptionsJson()
+ *
+ * @see RealCalendar
+ */
+final class Calendar extends Facade
 {
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
     protected static function getFacadeAccessor()
     {
         return 'laravel-fullcalendar';
